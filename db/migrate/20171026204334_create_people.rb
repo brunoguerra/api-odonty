d3f08c2f53a9fe8e9658778247ad7d6106f4513d
Jonathan Bruno <jonathanbruno.ueg@gmail.com>
2017-10-26 18:44:15 -0200

    creates people model to store all people data on the system

---
+++ db/migrate/20171026204334_create_people.rb
@@ -0,0 +1,14 @@
+class CreatePeople < ActiveRecord::Migration[5.1]
+  def change
+    create_table :people do |t|
+      t.string :name
+      t.date :birthdate
+      t.string :cpf
+      t.string :rg
+      t.integer :gender
+      t.integer :phone
+
+      t.timestamps
+    end
+  end
+end
