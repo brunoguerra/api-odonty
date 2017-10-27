6d35ffbd9fd6c4352990fa077e718f425db48e93
Jonathan Bruno <jonathanbruno.ueg@gmail.com>
2017-10-27 15:41:43 -0200

    create additional models tests

--- test/models/patient_test.rb
+++ test/models/patient_test.rb
@@ -5,7 +5,7 @@ class PatientTest < ActiveSupport::TestCase
   test 'should have the necessary required validators' do
     patient = Patient.new
     assert_not patient.valid?
-    assert_equal %i[person], patient.errors.keys
+    assert_equal %i[person additional], patient.errors.keys
   end
 
 end
