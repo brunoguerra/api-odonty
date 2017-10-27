18c2d25c4662febfaf3d4e413c962bfff00962b8
Jonathan Bruno <jonathanbruno.ueg@gmail.com>
2017-10-26 22:25:56 -0200

    add custom cpf validation

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,15 @@
+require "cpf_cnpj"
+
 class Person < ApplicationRecord
+  enum gender: [:male, :female]
   validates :name, :birthdate, :cpf, :rg, :gender, :presence => true
+
+  def cpf=(cpf)
+    write_attribute(:cpf, CPF.stripped(cpf)) if CPF.valid?(cpf)
+  end
+  
+  def cpf
+    read_attribute(:cpf)
+  end
+
 end
