c0b270ca06610a44b6243c29f979b6ffbdf82217
Jonathan Bruno <jonathanbruno>
2017-10-27 23:15:50 -0200

    add controller test file

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -2,10 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Person, type: :model do
   # Association test
-  # ensure Todo model has a 1:m relationship with the Item model
   it { should have_one(:address).dependent(:destroy) }
+
   # Validation tests
-  
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:gender) }
   it { should validate_presence_of(:birthdate) }
