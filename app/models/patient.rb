34e310266075695b689b51c9addc117783393850
Jonathan Bruno <jonathanbruno.ueg@gmail.com>
2017-11-04 21:49:37 -0200

    save

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -5,7 +5,7 @@ class Patient < ApplicationRecord
   self.primary_key = "person_id"
   
   belongs_to :person, foreign_key: "person_id", :required => true
-  has_one :additional, validate: true , :required => true
+  has_one :additional, validate: true #, :required => true
   has_one :anamnesis
 
   accepts_nested_attributes_for :person
